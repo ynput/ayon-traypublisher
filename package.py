54bec51d8cbe2ac940367e7858d866ba6d4dc918
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-16 10:56:29 +0200

    define host name

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "traypublisher"
 title = "TrayPublisher"
 version = "0.2.7+dev"
-
+app_host_name = "traypublisher"
 client_dir = "ayon_traypublisher"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
