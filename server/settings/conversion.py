85e048144b8ac3d5bb9ca800e0fcb0fcc777e326
Mustafa Jafar <mustafataherzaky@outlook.com>
2024-11-28 15:34:15 +0200

    enhance _convert_simple_creators_0_2_9
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- server/settings/conversion.py
+++ server/settings/conversion.py
@@ -5,12 +5,8 @@ def _convert_simple_creators_0_2_9(overrides):
     if "simple_creators" not in overrides:
         return
     
-    if overrides.get("create") is None:
-        overrides.update({
-            "create": {}
-        })
-    
-    overrides["create"]["simple_creators"] = overrides.pop("simple_creators")
+    create_overrides = overrides.setdefault("create", {})
+    create_overrides["simple_creators"] = overrides.pop("simple_creators")
 
 
 def _convert_editorial_creators_0_2_9(overrides):
