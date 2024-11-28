601c823bffbc13b57e6b6d0e5a6e4536132128c7
Mustafa Jafar <mustafataherzaky@outlook.com>
2024-11-28 15:33:59 +0200

    enhance _convert_editorial_creators_0_2_9
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- server/settings/conversion.py
+++ server/settings/conversion.py
@@ -17,12 +17,8 @@ def _convert_editorial_creators_0_2_9(overrides):
     if "editorial_creators" not in overrides:
         return
     
-    if overrides.get("create") is None:
-        overrides.update({
-            "create": {}
-        })
-    
-    overrides["create"]["editorial_creators"] = overrides.pop("editorial_creators")
+    create_overrides = overrides.setdefault("create", {})
+    create_overrides["editorial_creators"] = overrides.pop("editorial_creators")
 
 
 def convert_settings_overrides(
