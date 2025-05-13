6ec877d809007b0c9b03d8a26a3f91e451adc63c
Roy Nieterau <roy_nieterau@hotmail.com>
2025-05-13 22:04:22 +0200

    Improve texture batch creator to be more dynamic and allow choosing what creator type to create for

--- client/ayon_traypublisher/plugins/create/create_texture_batch.py
+++ client/ayon_traypublisher/plugins/create/create_texture_batch.py
@@ -1,36 +1,36 @@
-import copy
+import inspect
 import os
-import clique
 from typing import Optional
+
+import clique
 
 from ayon_core.lib import (
     FileDef,
     BoolDef,
-)
-from ayon_core.pipeline import (
-    CreatedInstance,
+    EnumDef,
+    TextDef,
+    UILabelDef,
+    UISeparatorDef,
 )
 
 from ayon_traypublisher.api.plugin import TrayPublishCreator
 
 
-class TextureSetBatchCreator(TrayPublishCreator):
+class BatchInstanceCreator(TrayPublishCreator):
     """Creates instances from texture sets.
 
     Intended for full texture sets for an asset.
     """
-    identifier = "texture_set_batch"
-    label = "Textures"
-    product_type = "texture"
-    description = "Publish batch of textures"
+    identifier = "batch_instances"
+    label = "Batch Split"
+    product_type = "batch"
+    description = "Publish batch of files as separate instances"
 
     # Position batch creator after simple creators
+    icon = "mdi.folder-multiple-image"
     order = 110
 
     extensions = {"png", "jpg", "jpeg", "tiff", "tif", "exr"}
-
-    def get_icon(self):
-        return "fa.file"
 
     def create(self, product_name, data, pre_create_data):
         file_paths = pre_create_data.get("filepath")
@@ -38,17 +38,32 @@ class TextureSetBatchCreator(TrayPublishCreator):
             return
 
         folder_path: str = data["folderPath"]
-        task_name: Optional[str] = data.get("taskName")
-        project_name = self.create_context.get_current_project_name()
+        task_name: Optional[str] = data.get("task")
         folder_entity = self.create_context.get_folder_entity(folder_path)
         task_entity = self.create_context.get_task_entity(folder_path,
                                                           task_name)
 
+        # All files
+        common_prefix: str = ""
+        if pre_create_data.get("strip_common_prefix"):
+            files = []
+            for file_info in file_paths:
+                files.extend(file_info["filenames"])
+            common_prefix = os.path.commonprefix(files)
+        self.log.debug("Found common prefix: %s", common_prefix)
+
         # Process the filepaths to individual instances
         for file_info in file_paths:
-            instance_data = copy.deepcopy(data)
             filenames = file_info["filenames"]
-            collections, remainder = clique.assemble(filenames)
+
+            # clique.PATTERNS["frames"] but also allow `_` before digits
+            pattern = r"[._](?P<index>(?P<padding>0*)\d+)\.\D+\d?$"
+            collections, remainder = clique.assemble(
+                filenames,
+                minimum_items=1,
+                assume_padded_when_ambiguous=True,
+                patterns=[pattern],
+            )
             if collections:
                 assert len(collections) == 1
                 basename = collections[0].format("{head}").rstrip("._")
@@ -56,38 +71,35 @@ class TextureSetBatchCreator(TrayPublishCreator):
                 assert len(remainder) == 1
                 basename = os.path.splitext(remainder[0])[0]
 
-            # TODO: Improve parsing of names
-            # TODO: Detect images of single UDIM tile so that it does not
-            #   include the UDIM tile number in the variant name
-            # TODO: Pass on the files!
-            #filepath = os.path.join(file_info["directory"], file_name)
-            #instance_data["creator_attributes"] = {"filepath": filepath}
+            if pre_create_data["strip_common_prefix"]:
+                # Remove common prefix from the variant name
+                basename = basename.removeprefix(common_prefix)
 
-            variant = basename
-            instance_data["variant"] = variant
-            product_name = self.get_product_name(
-                project_name,
-                folder_entity,
-                task_entity,
-                variant,
+            prefix = pre_create_data.get("prefix", "")
+            suffix = pre_create_data.get("suffix", "")
+            variant = "{}{}{}".format(prefix, basename, suffix)
+
+            self.create_context.create(
+                creator_identifier=pre_create_data["creator_type"],
+                folder_entity=folder_entity,
+                task_entity=task_entity,
+                variant=variant,
+                pre_create_data={
+                    "representation_files": [file_info]
+                }
             )
-
-            # Create new instance
-            new_instance = CreatedInstance(self.product_type, product_name,
-                                           instance_data, self)
-            self._store_new_instance(new_instance)
-
-    def get_instance_attr_defs(self):
-        return [
-            BoolDef(
-                "add_review_family",
-                default=False,
-                label="Review"
-            )
-        ]
 
     def get_pre_create_attr_defs(self):
-        # Use same attributes as for instance attributes
+        from ayon_traypublisher.api.plugin import SettingsCreator
+
+        items = []
+        for creator in self.create_context.creators.values():
+            if isinstance(creator, SettingsCreator):
+                items.append({
+                    "label": creator.label,
+                    "value": creator.identifier
+                })
+
         return [
             FileDef(
                 "filepath",
@@ -95,18 +107,59 @@ class TextureSetBatchCreator(TrayPublishCreator):
                 single_item=False,
                 extensions=self.extensions,
                 allow_sequences=True,
-                label="Filepath"
+                label="Files"
             ),
+            EnumDef(
+                "creator_type",
+                items=items,
+                label="Publish Type",
+                default="settings_render",
+                tooltip=(
+                    "The Simple Creator (publish type) to create instances "
+                    "with."
+                )
+            ),
+            UISeparatorDef("_additionals"),
+            UILabelDef("<b>Variant Name Options</b>"),
             BoolDef(
-                "add_review_family",
+                "strip_common_prefix",
                 default=True,
-                label="Review"
-            )
+                label="Strip Common Prefix",
+                tooltip=(
+                    "Remove common prefix from the variant name.\n\n"
+                    "This is useful when publishing a batch of textures "
+                    "for an asset that all start with the asset name.\n"
+                    "By stripping the common prefix, the variant name will "
+                    "then exclude the asset name."
+                )
+            ),
+            TextDef(
+                "prefix",
+                default="",
+                label="Add prefix",
+                tooltip=(
+                    "Add a common prefix to all variant names."
+                )
+            ),
+            TextDef(
+                "suffix",
+                default="",
+                label="Add suffix",
+                tooltip=(
+                    "Add a common suffix to all variant names."
+                )
+            ),
         ]
 
     def get_detail_description(self):
-        return """# Publish batch of textures for an asset
+        return inspect.cleandoc("""# Publish batch of files
 
-        It can be any amount of textures for the asset. The texture filename
-        will be used to define the "Variant" name.
-        """
+        This can be used to quickly create publish instances for a set of files
+        where the filenames then define the variant names. For example, using
+        a texture set:
+        - asset_diffuse.1001-1004.exr
+        - asset_specular.1001-1004.exr
+        
+        The batch instance creator can then quickly generate a "diffuse" and
+        "specular" instance accordingly.
+        """)
