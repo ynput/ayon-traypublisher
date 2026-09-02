9f8bf9014e066755239a38d6da9ae18dda21882f
Tadeáš Hejnic <65614308+tadeas-hejnic@users.noreply.github.com>
2026-09-02 14:28:42 +0200

    delete unused code
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_traypublisher/batch_parsing.py
+++ client/ayon_traypublisher/batch_parsing.py
@@ -95,7 +95,6 @@ def parse_with_version(
         )
         if matching_folder_entities:
             version_number = int(_version_number)
-            # matching_folder_entity = matching_folder_entity.pop()
 
     return matching_folder_entities, version_number
 
